62cd1bc0769c2aba7b929a1ba25c090963887be9
André Luis Gonçalves Oliveira <algo1305@gmail.com>
2019-06-20 18:56:17 -0300

    Corrige erros de sintaxe e warnings

--- src/app/auction-add/auction-add.component.ts
+++ src/app/auction-add/auction-add.component.ts
@@ -1,8 +1,8 @@
-import { Component, OnInit,Input, ViewChild, ElementRef, EventEmitter, Output } from '@angular/core';  
-import { NgForm } from '@angular/forms';  
+import { Component, OnInit, Input, ViewChild, ElementRef, EventEmitter, Output } from '@angular/core';
+import { NgForm } from '@angular/forms';
 import { Auction } from 'src/Models/Auction';
-import { Router } from '@angular/router';  
-import {AuctionDataService} from '../DataService/AuctionDataService';
+import { Router } from '@angular/router';
+import { AuctionDataService } from '../DataService/AuctionDataService';
 
 @Component({
   selector: 'app-auction-add',
@@ -11,35 +11,37 @@ import {AuctionDataService} from '../DataService/AuctionDataService';
 })
 export class AuctionAddComponent implements OnInit {
 
-  @Input() cleardata: boolean = false;
-  @Output() nameEvent: new EventEmitter<string>();
-  objtempauction : Auction;
-  @Input() objauc :Auction= new Auction();;
-  @ViewChild('closeBtn') cb: ElementRef;
 
-  constructor(private dataService:AuctionDataService, private route:Router) { 
-  
-  }
-  ngOnInit() {
-    }
-  Register(regForm:NgForm){
-    this.objtempauction= new Auction();
-    this.objtempauction.auctionName= regForm.value.auctionName;
-    this.objtempauction.initialBet=regForm.value.initialBet;
-    this.objtempauction.usedItem=regForm.value.usedItem;
-    this.objtempauction.auctioneer = regform.value.auctioneer;
-    this.objtempauction.startDate = regform.value.startDate;
-    this.objtempauction.endDate = regform.value.endDate;
+  @Input() cleardata = false;
+  @Output() nameEvent= new EventEmitter<string>();
+  objtempauction: Auction;
+  @Input() objauc: Auction = new Auction(); 
+  @ViewChild('closeBtn', {static: false}) cb: ElementRef;
 
-  this.dataService.AddAuction(this.objtempauction).subscribe(res =>{
-    alert("Leilao registrado com sucesso!");
+constructor(private dataService: AuctionDataService, private route: Router) {
+
+}
+ngOnInit() {
+
+}
+Register(regForm: NgForm){
+  this.objtempauction = new Auction();
+  this.objtempauction.auctionName = regForm.value.auctionName;
+  this.objtempauction.initialBet = regForm.value.initialBet;
+  this.objtempauction.usedItem = regForm.value.usedItem;
+  this.objtempauction.auctioneer = regForm.value.auctioneer;
+  this.objtempauction.startDate = regForm.value.startDate;
+  this.objtempauction.endDate = regForm.value.endDate;
+
+  this.dataService.AddAuction(this.objtempauction).subscribe(res => {
+    alert('Leilao registrado com sucesso!');
     this.TakeHome();
   }
   )
-  }
-  TakeHome(){
-    this.nameEvent.emit("ccc");
-    this.cb.nativeElement.click();
-    this.route.navigateByUrl('');
-  }
+}
+TakeHome(){
+  this.nameEvent.emit('ccc');
+  this.cb.nativeElement.click();
+  this.route.navigateByUrl('');
+}
 }
